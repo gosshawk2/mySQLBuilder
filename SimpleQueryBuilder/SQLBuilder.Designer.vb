741e0b2145fd846afa274191537ec9e261a4f0d2
Daniel Goss <gosshawk2@yahoo.co.uk>
2020-03-27 18:06:31 +0000

    SQLBuilder v1.0 First commit DG 27-03-2020 18:06

--- SimpleQueryBuilder/SQLBuilder.Designer.vb
+++ SimpleQueryBuilder/SQLBuilder.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class SQLBuilder
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,6 +23,12 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.gbTOP = New System.Windows.Forms.GroupBox()
+        Me.pnlMain = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.txtSQLQuery = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.pnlMain.SuspendLayout()
         Me.SuspendLayout()
         '
         'gbTOP
@@ -31,21 +37,78 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.gbTOP.Location = New System.Drawing.Point(1, 2)
         Me.gbTOP.Name = "gbTOP"
-        Me.gbTOP.Size = New System.Drawing.Size(1054, 108)
+        Me.gbTOP.Size = New System.Drawing.Size(1236, 66)
         Me.gbTOP.TabIndex = 0
         Me.gbTOP.TabStop = False
         '
-        'Form1
+        'pnlMain
+        '
+        Me.pnlMain.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlMain.Controls.Add(Me.Label2)
+        Me.pnlMain.Controls.Add(Me.Label1)
+        Me.pnlMain.Controls.Add(Me.txtSQLQuery)
+        Me.pnlMain.Controls.Add(Me.ListBox1)
+        Me.pnlMain.Location = New System.Drawing.Point(1, 74)
+        Me.pnlMain.Name = "pnlMain"
+        Me.pnlMain.Size = New System.Drawing.Size(1236, 486)
+        Me.pnlMain.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(159, 36)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(217, 264)
+        Me.ListBox1.TabIndex = 0
+        '
+        'txtSQLQuery
+        '
+        Me.txtSQLQuery.Location = New System.Drawing.Point(159, 357)
+        Me.txtSQLQuery.Multiline = True
+        Me.txtSQLQuery.Name = "txtSQLQuery"
+        Me.txtSQLQuery.Size = New System.Drawing.Size(767, 45)
+        Me.txtSQLQuery.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(76, 360)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "SQL Query:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(76, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Select Table:"
+        '
+        'SQLBuilder
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1056, 298)
+        Me.ClientSize = New System.Drawing.Size(1238, 564)
+        Me.Controls.Add(Me.pnlMain)
         Me.Controls.Add(Me.gbTOP)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Name = "SQLBuilder"
+        Me.Text = "SQL Builder"
+        Me.pnlMain.ResumeLayout(False)
+        Me.pnlMain.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents gbTOP As GroupBox
+    Friend WithEvents pnlMain As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSQLQuery As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
 End Class
